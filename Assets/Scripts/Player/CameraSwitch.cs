05c806c7f9322fa6fc94bc09b56e386e53219b6b
Seongjune <bini060315@gmail.com>
2025-05-10 15:21:32 +0900

    락온 기능 만듬

--- Assets/Scripts/Player/CameraSwitch.cs
+++ Assets/Scripts/Player/CameraSwitch.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 
-public class CameraSwitcher : MonoBehaviour
+public class CameraSwitch : MonoBehaviour
 {
     public PlayerCamera playerCamera;  // 평소 카메라
     public CameraLock actionCamera;  // 액션 모드 카메라
+    public LockOn lockOn;
     private Quaternion cameraRotation;
 
     private bool isActionMode = false;
+    public bool isLockOnMode = false;
 
     void Start()
     {
@@ -15,7 +17,11 @@ public class CameraSwitcher : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.H)) // H 키를 누르면 전환
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.H) && !isLockOnMode) // H 키를 누르면 전환
         {
             isActionMode = !isActionMode; // isActionMode의 값을 반대로 바꾸는 것 true -> false , false -> true
 
@@ -32,6 +38,31 @@ public class CameraSwitcher : MonoBehaviour
                 Cursor.lockState = CursorLockMode.None;  // 마우스 커서 다시 보이게
                 transform.rotation = cameraRotation;
             }
+        }
+
+        if (Input.GetKeyDown(KeyCode.G) && !isActionMode)
+        {
+            if (Vector3.Distance(lockOn.boss.transform.position, lockOn.player.transform.position) <= 25 ||
+                Vector3.Distance(lockOn.dummy.transform.position, lockOn.player.transform.position) <= 25)
+            {
+                isLockOnMode = !isLockOnMode;
+
+                if (isLockOnMode)
+                {
+                    playerCamera.enabled = false;
+                    lockOn.enabled = true;
+                }
+                else
+                {
+                    playerCamera.enabled = true;
+                    lockOn.enabled = false;
+                }
+            }
+            else 
+            {
+                return;
+            }
+            
         }
     }
 }
--- Assets/Scripts/Player/LockOn.cs
+++ Assets/Scripts/Player/LockOn.cs
@@ -2,39 +2,57 @@ using UnityEngine;
 
 public class LockOn : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject boss;
+    public GameObject dummy;
     [SerializeField]
-    private GameObject player;
-    [SerializeField]
-    private GameObject boss;
-    [SerializeField]
-    private GameObject dummy;
-    
-    private bool isLockOn = false;
+    private Vector3 offset = new Vector3(0, 3, -5);
 
 
-    void Start()
+    public CameraSwitch cameraSwitch;
+
+
+    void Awake()
     {
-        player = gameObject;
+        player = GameObject.FindWithTag("Player");
         boss = GameObject.FindWithTag("Boss");
-        dummy = GameObject.FindWithTag("Dummy");   
+        dummy = GameObject.FindWithTag("Dummy");
     }
 
-    void Update()
+    void LateUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            if (isLockOn)
-            {
-                isLockOn = false;
-            }
+        CameaLockOn();
+    }
 
-            if (Vector3.Distance(boss.transform.position , player.transform.position) <= 25)
+    void CameaLockOn()
+    {
+        if (cameraSwitch.isLockOnMode)
+        {
+            if (Vector3.Distance(boss.transform.position, player.transform.position) <= 25)
             {
-                isLockOn = true;
+                Vector3 desiredPosition = player.transform.position + player.transform.rotation * offset;
+                transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * 10);
+                transform.LookAt(boss.transform.position + Vector3.up * 1.5f);
+
+                Vector3 directionToTarget = (boss.transform.position - player.transform.position).normalized;
+                directionToTarget.y = 0; // 수평 회전만 하도록 y 제거
+
+            
+                Quaternion targetRotation = Quaternion.LookRotation(directionToTarget);
+                player.transform.rotation = Quaternion.Slerp(player.transform.rotation, targetRotation, Time.deltaTime * 100);
             }
-            if (Vector3.Distance(dummy.transform.position , player.transform.position) <= 25)
+            if (Vector3.Distance(dummy.transform.position, player.transform.position) <= 25)
             {
-                isLockOn = true;
+                Vector3 desiredPosition = player.transform.position + player.transform.rotation * offset;
+                transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * 10);
+                transform.LookAt(dummy.transform.position + Vector3.up * 1.5f);
+
+                Vector3 directionToTarget = (dummy.transform.position - player.transform.position).normalized;
+                directionToTarget.y = 0; // 수평 회전만 하도록 y 제거
+
+
+                Quaternion targetRotation = Quaternion.LookRotation(directionToTarget);
+                player.transform.rotation = Quaternion.Slerp(player.transform.rotation, targetRotation, Time.deltaTime * 100);
             }
         }
     }
